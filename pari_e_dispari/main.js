99c7345fc14ee69a28af5cbde908c12dc2c7cfe0
Valeria Sozzi <valeriasozzi24@gmail.com>
2024-05-14 11:32:16 +0200

    add exercise pari e dispari

---
+++ pari_e_dispari/main.js
@@ -0,0 +1,51 @@
+/* 
+L'utente sceglie pari o dispari e inserisce un numero da 1 a 5 
+generiamo un numero random (da 1 a 5) per il pc (usando una funzione)
+sommiamo i due numeri
+stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+dichiariamo chi ha vinto
+*/
+
+let somma=0
+//chiediamo all'utente di scegliere tra pari e dispari
+const pariDispari = prompt('scegli se vuoi pari o dispari')
+console.log(pariDispari);
+
+//chiediamo di inserire un numero da 1 a 5 
+const userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
+console.log(userNumber);
+
+//funzione per generare un numero random per il pc da 1 a 5 
+function pcNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+let pc_number = pcNumber (1,5)
+console.log(pc_number);
+
+//somma dei due numeri
+
+somma = userNumber + pc_number
+console.log(somma);
+
+//stabilire se la somma è pari o dispari
+
+function isEven(sommaNumber) {
+    if (somma % 2 === 0 ){
+        return true;
+    } else {
+        return false
+    }
+}
+
+console.log(isEven(somma), somma);
+
+//dichiarare il vincitore
+
+if (pariDispari ==='pari' && isEven(somma)){
+    console.log('Hai vinto');
+}else if (pariDispari==='dispari' && !isEven(somma)){
+    console.log('Hai vinto');
+} else {
+    console.log('Hai perso');
+}
